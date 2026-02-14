64ddde17a4c8721b2a31bc4fd88c247c3fac09a2
sincerajn <sincerajn@outlook.com>
2026-02-14 17:14:26 +0800

    feat: add DefaultEnabled property to BottledAbilitySpec

--- Types/BottledAbilitySpec.cs
+++ Types/BottledAbilitySpec.cs
@@ -4,11 +4,13 @@ public sealed class BottledAbilitySpec(
     string abilityDefName,
     string packageId,
     AbilityCategory defaultCategory,
-    int defaultCharges = 1) {
+    int defaultCharges = 1,
+    bool defaultEnabled = true) {
     public string AbilityDefName { get; } = abilityDefName;
     public string PackageId { get; } = packageId;
     public AbilityCategory DefaultCategory { get; } = defaultCategory;
     public int DefaultCharges { get; } = defaultCharges;
+    public bool DefaultEnabled { get; } = defaultEnabled;
 
     public string JarDefName => $"VortexBA_{AbilityDefName}";
 }
